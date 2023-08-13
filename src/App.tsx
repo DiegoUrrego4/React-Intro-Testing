4ce7cb72a8ccdfa4df9453b8556391c355e9e8ec
Diego Urrego <diegourrg@gmail.com>
2023-08-12 23:52:03 -0500

    test: Tests added
    I added tests for <FirstApp /> using the object and methods returns from render and using the object screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
-import { FirstApp } from './FirstApp';
-// import { Counter } from './Counter';
+// import { FirstApp } from './FirstApp';
+import { Counter } from './Counter';
 
 function App() {
   return (
     <>
-      {/* <Counter /> */}
-      <FirstApp title='Hola, Soy Vegeta' />
+      <Counter />
+      {/* <FirstApp title="Hola, Soy Vegeta" /> */}
     </>
   );
 }
--- tests/FirstApp.test.tsx
+++ tests/FirstApp.test.tsx
@@ -3,18 +3,30 @@ import { render } from '@testing-library/react';
 import { FirstApp } from '../src/FirstApp';
 
 describe('Pruebas en <FirstApp />', () => {
-  test('Debe de hacer match con el snapshot()', () => {
-    const title = 'Hola, soy Gokú';
-    const { container } = render(<FirstApp title={title} />);
-    expect(container).toMatchSnapshot();
-  });
+  // test('Debe de hacer match con el snapshot()', () => {
+  //   const title = 'Hola, soy Gokú';
+  //   const { container } = render(<FirstApp title={title} />);
+  //   expect(container).toMatchSnapshot();
+  // });
 
   test('Debe de mostrar el titulo en un h1', () => {
     const title = 'Hola, soy Gokú';
-    const { container, getByText } = render(<FirstApp title={title} />);
+    const { container, getByText, getByTestId } = render(
+      <FirstApp title={title} />
+    );
     // Con esto me aseguro que exista el título
     expect(getByText(title)).toBeTruthy();
-    const h1 = container.querySelector('h1');
-    expect(h1?.innerHTML).toContain(title);
+    // const h1 = container.querySelector('h1');
+    // expect(h1?.innerHTML).toContain(title);
+    expect(getByTestId('test-title').innerHTML).toContain(title);
+  });
+
+  test('Debe de mostrar el subtitulo mostrado por props', () => {
+    const title = 'Hola, soy Gokú';
+    const subTitle = 'Soy un subtitulo';
+    const { getAllByText } = render(
+      <FirstApp title={title} subTitle={subTitle} />
+    );
+    expect(getAllByText(subTitle).length).toBe(2); // Esto devuelve un array de elementos
   });
 });
